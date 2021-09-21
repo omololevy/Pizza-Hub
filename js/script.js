531693173dbe6fc59ba2e1dc4c5e418ec0fc8b54
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-09-21 19:13:13 +0300

    Try solving the arithmetic amount calculation using switch case

--- js/script.js
+++ js/script.js
@@ -20,7 +20,40 @@ $(document).ready(function(){
         });
         console.log(ptopping.join(", "));
     
-        
+        switch(psize){
+        case "0":
+            price =0;
+        break;
+        case "large":
+            price = 1200;
+            console.log(price);
+        break;
+        case "medium":
+            price = 850;
+            console.log("The price is "+price);
+        break;
+        case "small":
+            price = 600;
+            console.log(price);
+        default:
+            console.log("error"); 
+        }
+        switch(pcrust){
+            case "0":
+            crust_price = 0;
+            break;
+            case "Crispy":
+            crust_price = 200;
+            break;
+            case "Stuffed":
+            crust_price = 250;
+            break;
+            case "Gluten-free":
+            crust_price = 180;
+            break;
+            default:
+            console.log("No price"); 
+        }
         let topping_value = ptopping.length*50;
         console.log("toppins value" + topping_value);
     
@@ -56,7 +89,41 @@ $(document).ready(function(){
             $.each($("input[name='toppings']:checked"), function(){            
                 ptopping.push($(this).val());
             });
-            
+            console.log(ptopping.join(", "));
+            switch(psize){
+            case "0":
+                price =0;
+            break;
+            case "large":
+                price = 1200;
+                console.log(price);
+            break;
+            case "medium":
+                price = 850;
+                console.log("The price is "+price);
+            break;
+            case "small":
+                price = 600;
+                console.log(price);
+            default:
+                console.log("error"); 
+            }
+            switch(pcrust){
+                case "0":
+                crust_price = 0;
+                break;
+                case "Crispy":
+                crust_price = 200;
+                break;
+                case "Stuffed":
+                crust_price = 150;
+                break;
+                case "Gluten-free":
+                crust_price = 180;
+                break;
+                default:
+                console.log("No price"); 
+            }
             let topping_value = ptopping.length*50;
             console.log("toppins value" + topping_value);
             total = price + crust_price + topping_value;
